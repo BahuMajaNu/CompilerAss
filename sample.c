8ddbda71371cb882acff74064759a1d11aa20a88
BahuMajaNu <106115048@nitt.edu>
2018-08-25 17:21:09 +0530

    Q2, Q3 added

---
+++ sample.c
@@ -0,0 +1,12 @@
+for(int a=0;a<10;a+=50){
+	if(){
+	}
+		bkacm;
+		lulva;
+		kjhvkds;
+
+}
+printf("ab%dc%c",b,p);
+luk
+scanf("%d %d",&b,&v);
+lulwa
---
+++ target.c
@@ -0,0 +1,16 @@
+int a=0;
+while(a<10)
+{
+	if(){
+	}
+		bkacm;
+		lulva;
+		kjhvkds;
+
+a+=50;
+}
+
+printf("ab%dc%c",b,p);
+luk
+scanf("%d %d",&b,&v);
+lulwa
